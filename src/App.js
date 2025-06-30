e20ae72440bda9a307b49bddfbcb680e39ce7013
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-06-30 17:05:27 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './index.scss';
 
-// VARIANT 1
-
 function App() {
    const [open, setOpen] = React.useState(false);
 
@@ -12,53 +10,31 @@ function App() {
             ✨ Открыть окно
          </button>
 
-         <div className={`overlay animated ${open ? 'show' : ''}`}>
-            <div className="modal">
-               <svg
-                  onClick={() => setOpen(false)}
-                  height="200"
-                  viewBox="0 0 200 200"
-                  width="200"
-               >
-                  <title />
-                  <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-               </svg>
-               <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-            </div>
-         </div>
+         <Modal open={open} setOpen={setOpen}>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+            <h3>Это модальное окно</h3>
+         </Modal>
       </div>
    );
 }
 
-// VARIANT 2
-
-// function App() {
-//    const [open, setOpen] = React.useState(false);
-
-//    return (
-//       <div className="App">
-//          <button onClick={() => setOpen(true)} className="open-modal-btn">
-//             ✨ Открыть окно
-//          </button>
-
-//          {open && (
-//             <div className="overlay">
-//                <div className="modal">
-//                   <svg
-//                      onClick={() => setOpen(false)}
-//                      height="200"
-//                      viewBox="0 0 200 200"
-//                      width="200"
-//                   >
-//                      <title />
-//                      <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-//                   </svg>
-//                   <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-//                </div>
-//             </div>
-//          )}
-//       </div>
-//    );
-// }
+const Modal = ({ open, setOpen, children }) => {
+   return (
+      <div className={`overlay animated ${open ? 'show' : ''}`}>
+         <div className="modal">
+            <svg
+               onClick={() => setOpen(false)}
+               height="200"
+               viewBox="0 0 200 200"
+               width="200"
+            >
+               <title />
+               <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            {children}
+         </div>
+      </div>
+   );
+};
 
 export default App;
